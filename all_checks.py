844b13d632d2856e16ce4553794066ed24bb2aa4
Elliot Wilens <wilensel@gmail.com>
2020-07-29 11:35:34 -0500

    added check_disk_full function to match instructor

--- all_checks.py
+++ all_checks.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
 import os
+import shutil
 import sys
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
+def check_disk_full(disk, min_absolute, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise"""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return True
+    return False
+
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
+    if check_disk_full("/", 2, 10):
+        print("Disk full.")
+        sys.exit(1)
     else:
         print("Everything ok.")
-        sys.exit(2)
+        sys.exit(0)
         
 main()
