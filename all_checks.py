2aac5c7b60776b754db0c225a758a6b8db952b5e
Elliot Wilens <wilensel@gmail.com>
2020-07-30 11:47:30 -0500

    Revert "Add a simple network connectivity check"
    Want to make changes as another user before committing this
    
    This reverts commit ee90796a701d8bffd38179ea8437365391edb901.

--- all_checks.py
+++ all_checks.py
@@ -2,7 +2,6 @@
 import os
 import shutil
 import sys
-import socket
 
 def check_reboot():
     """Returns True if the PC has a pending reboot"""
@@ -22,19 +21,10 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise"""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
-def check_no_network():
-    """Returns True if it fails to resolve Google's URL, False otherwise"""
-    try:
-        socket.gethostbyname("www.google.com")
-        return False
-    except:
-        return True
-
 def main():
     checks = [
             (check_reboot, "Pending reboot"),
             (check_root_full, "Root partition full"),
-            (check_no_network, "No working network.")
     ]
     everything_ok = True
     for check, msg in checks:
