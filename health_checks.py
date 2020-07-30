5f13b550f34b5e4c53592643ba8cd365b802abdb
Elliot Wilens <wilensel@gmail.com>
2020-07-30 11:49:09 -0500

    Add a simple network connectivity check

--- health_checks.py
+++ health_checks.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import sys
 import psutil
+import socket
 
 def check_reboot():
     """Returns True if the PC has a pending reboot"""
@@ -23,13 +24,23 @@ def check_root_full():
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 def check_cpu_constrained():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
     return psutil.cpu_percent(1) > 75
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
 
 def main():
     checks = [
             (check_reboot, "Pending reboot"),
             (check_root_full, "Root partition full"),
-            (check_cpu_constrained, "CPU constrained.")
+            (check_cpu_constrained, "CPU load too high."),
+            (check_no_network, "No working network."),
     ]
     everything_ok = True
     for check, msg in checks:
